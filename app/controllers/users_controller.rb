1e35406540996fca29c22ae8e82c521fb559a59b
keisuke.okamura <cointoss69@icloud.com>
2023-10-09 06:42:24 +0000

    kadai-tasklist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
   
   def index
     @pagy, @users = pagy(User.order(id: :desc), items: 25)
@@ -20,7 +19,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
